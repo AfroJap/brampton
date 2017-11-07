71be344a2e29bb3e76dffe688f27b630d3c99bde
gursimranshinger <gursmran.shinger@gmail.com>
2017-11-07 09:59:57 +0000

    Work in progress

--- 1darray.py
+++ 1darray.py
@@ -1,24 +1,25 @@
 # DECLARE a : ARRAY[0..9] OF INTEGER
 
-lQ = "bum"
-a = [7, 5, 6, 4, 9, 10, 8, 3, 1, 2]
-i = 0
-
-for y in range(1, 11):
-    print a[i]
-    i = i + 1
+for q in range[0, 9]:
+    print("Enter a whole number")
+    a[q] = input()
 
 u = 0
 numb = 0
-for y in range(1, 100):
-    port = u + 1
-    if a[u] > a[u + 1]:
-        a[u] = numb
-        a[u] = a[u + 1]
-        a[u+1] = numb
-    u = u + 1
+for r in range(9, 0, -1):
+    numb = 0
+    for y in range(0, r):
+        port = y + 1
+        if a[y] > a[y + 1]:
+            numb = a[y]
+            a[y] = a[y + 1]
+            a[y+1] = numb
 
 for y in range(1, 11):
     print a[i]
     i = i + 1
-input()
+
+
+
+
+# to loop from 9 down to 0:for index in range(9, -1, -1)
